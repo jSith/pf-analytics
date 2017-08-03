b2939efdcb11d75073d0685d18502bc38a9e4e19
jSith <30677679+jSith@users.noreply.github.com>
2017-08-02 21:18:16 -0500

    Enum of creature types

---
+++ Type.java
@@ -0,0 +1,19 @@
+
+public enum Type {
+
+	aberration,
+	animal,
+	construct,
+	dragon,
+	fey,
+	humanoid,
+	magicalBeast,
+	monstrousHumanoid,
+	ooze, 
+	outsider, 
+	plant, 
+	undead, 
+	vermin
+	
+	
+}
